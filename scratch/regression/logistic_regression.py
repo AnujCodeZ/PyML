8d2e656c4be6c83d13da519936d2f815dc8b73de
AnujCodeZ <anujrana.sid55@gmail.com>
2020-09-11 23:02:23 +0530

    Update LogReg

--- scratch/regression/logistic_regression.py
+++ scratch/regression/logistic_regression.py
@@ -5,32 +5,21 @@ import numpy as np
 class LogReg:
 
     # Constructor
-    def __init__(self, num_features, num_labels):
+    def __init__(self, num_features):
 
-        self.weights = np.random.rand(num_features, num_labels) * 0.01
-        self.bias = np.zeros((1, num_labels))
-        self.num_features = num_features
-        self.num_labels = num_labels
+        self.weights = np.zeros((num_features, 1))
+        self.bias = 0
 
     # Activations
     def sigmoid(self, Z):
 
         return 1. / (1. + np.exp(-Z))
-
-    def softmax(self, Z):
-
-        return np.exp(Z) / np.sum(Z)
     
     # Forward pass
     def forward(self, X):
-
         Z = np.matmul(X, self.weights) + self.bias
+        return self.sigmoid(Z)
         
-        if self.num_labels == 1:
-            A = self.sigmoid(Z)
-        else:
-            A = self.softmax(Z)
-        return A
     
     # Cost function
     def compute_cost(self, A, Y):
@@ -59,19 +48,16 @@ class LogReg:
     
     # Prediction
     def predict(self, X):
-
         A = self.forward(X)
-        Y_hat = A > 0.5
-
+        Y_hat = np.where(A>0.5, 1., 0.)
         return Y_hat
     
     # Accuracy
     def accuracy(self, X, Y):
-
         Y_hat = self.predict(X)
         accuracy = np.sum(Y_hat == Y) / X.shape[0]
-
         return accuracy
+    
     # Training.
     def train(self, x_train, y_train, iterations=3000, lr=0.03):
         losses = []

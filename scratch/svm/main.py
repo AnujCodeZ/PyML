ada4584a735a64762ec7c1026a57ca79233c6c39
AnujCodeZ <anujrana.sid55@gmail.com>
2020-09-14 22:01:54 +0530

    SVM

--- scratch/svm/main.py
+++ scratch/svm/main.py
@@ -1,38 +1,36 @@
 import numpy as np
+from tqdm import tqdm
 
 
 class SVM:
 
-    def __init__(self, features, lambda_param):
+    def __init__(self, features, lambda_param=0.01):
         self.lambda_param = lambda_param
         self.weights = np.zeros(features)
         self.bias = 0
         self.dw = np.zeros_like(self.weights)
         self.db = 0
-
     def forward(self, x):
         z = np.dot(x, self.weights) + self.bias
-        y_hat = np.where(z >= 0, 1., 0.)
-        return z, y_hat
-
-    def cost(self, y, z):
-        cost_1 = np.where(z>=1, 0., z)
-        cost_1 = np.where(z<=-1, 0., z)
-        loss = (np.dot(y.T, cost_1)
-                np.dot((1 - y).T, cost_0))
-        loss = loss / self.lambda_param
-        loss = loss + (1/2) * np.dot(self.weights.T, self.weights)
-        return loss
+        return np.sign(z)
     
-    def backward(self, x, y, y_hat, z):
-        if z >= 1 or z <= -1:
-            self.dw = self.weights
+    def backward(self, x_i, y_i):
+        condition = y_i * (np.dot(x_i, self.weights)-self.bias) >= 1
+        if condition:
+            self.dw = 2 * self.lambda_param * self.weights
             self.db = 0
         else:
-            self.dw = np.dot(x.T, ((-y/z) + (1 - y)/(1-z))) + \
-                self.weights
-            self.db = np.sum(((-y/z) + (1 - y)/(1-z)), axis=1)
+            self.dw = 2 * self.lambda_param * self.weights - \
+                np.dot(x_i, y_i)
+            self.db = y_i
     
-        
-        
-        
+    def update(self, lr):
+        self.weights = self.weights - lr * self.dw
+        self.bias = self.bias - lr * self.db
+    
+    def train(self, x, y, lr=3e-3, epochs=1000):
+        y = np.where(y <= 0, -1., 1.)
+        for e in tqdm(range(epochs)):
+            for idx, x_i in enumerate(x):
+                self.backward(x_i, y[idx])
+                self.update(lr)

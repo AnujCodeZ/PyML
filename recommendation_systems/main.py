d8a4e4ebb95725c592d953d22266e0879a1fb454
AnujCodeZ <anujrana.sid55@gmail.com>
2020-10-12 23:13:25 +0530

    RecommendationUsingItems

--- recommendation_systems/main.py
+++ recommendation_systems/main.py
@@ -57,3 +57,41 @@ def user_based_suggestions(user_id, include_current_interests=False):
         return [(suggestion, weight)
                 for suggestion, weight in suggestions
                 if suggestion not in users_interests[user_id]]
+
+interest_user_matrix = [[user_interest_vector[j]
+                         for user_interest_vector in user_interest_vectors]
+                        for j, _ in enumerate(unique_interests)]
+
+interest_similarities = [[cosine_similarity(user_vetor_i, user_vector_j)
+                          for user_vector_j in interest_user_matrix]
+                         for user_vector_i in interest_user_matrix]
+
+def most_similar_interests_to(interest_id):
+    similarities = interest_similarities[interest_id]
+    pairs = [(unique_interests[other_interest_id], similarity)
+             for other_interest_id, similarity in enumerate(similarities)
+             if interest_id != other_interest_id and similarity > 0]
+    return sorted(pairs,
+                  key=lambda pair: pair[-1],
+                  reverse=True)
+
+def item_based_suggestions(user_id, include_current_interests):
+    suggestions = defaultdict(float)
+    user_interest_vector = user_interest_vectors[user_id]
+    for interest_id, is_interested in enumerate(user_interest_vector):
+        if is_interested == 1:
+            similar_interests = most_similar_interests_to(interest_id)
+            for interest, similarity in similar_interests:
+                suggestions[interest] += similarity
+    
+    suggestions = sorted(suggestions.items(),
+                         key=lambda pair: pair[-1],
+                         reverse=True)
+    
+    if include_current_interests:
+        return suggestions
+    else:
+        return [(suggestion, weight)
+                for suggestion, weight in suggestions
+                if suggestion not in users_interests[user_id]]
+
